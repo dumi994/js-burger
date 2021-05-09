9aac8e7a8dae5d9cd8fe673c4e69d337eda3bacf
dumi994 <dumi.9944@gmail.com>
2021-05-09 20:06:58 +0200

    struttura html

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+//Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
+//Consigli del giorno:
+//Create inizialmente un html base con gli elementi minimi necessari al funzionamento, 
+//passate poi alla parte JS ragionando come sempre a step. 
+//Prima la logica in italiano e poi traduciamo in codice. console.log() è nostro amico
+//. Quando tutto funziona passate alla parte visiva lavorando al css.
