5477c4d698f24b825810c4660ccba30fa44b8da6
dumi994 <dumi.9944@gmail.com>
2021-05-12 22:24:51 +0200

    ?_?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,26 @@
 //Il programma dovrà consentire di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
+var prezzoBase = 50;
+var salsa = 5;
+var aggiunta = 10;
+
+
+
+
+var userPomodoro = document.getElementById('pomodori').value;
+var userMustard = document.getElementById('pomodori').value;
+var userCheese = document.getElementById('pomodori').value;
+var userEgg = document.getElementById('pomodori').value;
+var userLettuce = document.getElementById('pomodori').value;
+var userKetchup = document.getElementById('pomodori').value;
+console.log(userKetchup);
+console.log(userEgg);
+/* function prezzoPanino(){
+    var panino = userSelect + prezzoBase;
+    console.log(panino);
+    if (userSelec){
+
+    }
+}; */
 //Consigli del giorno:
 //Create inizialmente un html base con gli elementi minimi necessari al funzionamento, 
 //passate poi alla parte JS ragionando come sempre a step. 
