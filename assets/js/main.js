3023aa56fcd07dfc1fa6826deea5497cdaf7a80a
dumi994 <dumi.9944@gmail.com>
2021-05-17 17:53:28 +0200

    tentativo somma elem selezionati al prezzo base

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,33 +3,27 @@ const prezzoBase = 50;
 const salsa = 5;
 const aggiunta = 10;
 
-document.getElementById('crea').addEventListener('click', function (){
-    let aggiunte = document.getElementById('userSelect').value;
-    console.log(aggiunte);
 
-    
-});
-let tomato = document.getElementById('tomato').value;
-let cheese = document.getElementById('cheese').value;
-let egg = document.getElementById('egg').value;
-let mustard = document.getElementById('mustard').value;
-let lettuce = document.getElementById('lettuce').value;
-let ketchup = document.getElementById('ketchup').value;
 
 
 var checkboxes = document.getElementsByClassName('ingredient-checkbox');
 console.log(checkboxes);
+let sum = 0;
 for (let i=0; i < checkboxes.length; i++){
   let checkedEl = checkboxes[i];
   console.log(checkedEl);
- 
-  
-  /* if (checkedEl == ){
-    checkedEl += prezzoBase
-  } */
+  let checkbox = checkboxes[i];
+  if(checkbox.checked == true) {
+    value = checkbox.value;
+    console.log(value += prezzoBase);
+  }
 }
- let valueEl = document.getElementsByClassName('ingredient-checkbox').value;
- console.log(valueEl);
+
+
+document.getElementById('crea').addEventListener('click', function (){
+    let aggiunte = document.getElementById('userSelect').value;
+    console.log(aggiunte);    
+});
 //Consigli del giorno:
 //Create inizialmente un html base con gli elementi minimi necessari al funzionamento, 
 //passate poi alla parte JS ragionando come sempre a step. 
